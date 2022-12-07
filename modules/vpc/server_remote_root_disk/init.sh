f328ece89edd9b4e0760fd88697e7d39eafec9be
Zukimorimkato <107054272+Zukimorimkato@users.noreply.github.com>
2022-12-07 08:49:59 +0300

    updated init.sh

--- modules/vpc/server_remote_root_disk/init.sh
+++ modules/vpc/server_remote_root_disk/init.sh
@@ -3,4 +3,5 @@ sudo apt-get -y update && sudo apt-get -y install nginx && sudo service nginx st
 sleep 3
 rm -rf /var/www/html/index.html
 sleep 1
-touch /var/www/html/index.html && ss -tuln  | awk '{print "<pre>"$5, $6"</pre>"}' >> /var/www/html/index.html && echo "<h1>Kokorin https://github.com/Zukimorimkato/terra</h1>" >> /var/www/html/index.html
+touch /var/www/html/index.html && echo "<h1>Kokorin https://github.com/Zukimorimkato/terra</h1>" >> /var/www/html/index.html
+ss -tuln  | awk 'NR>1 {print "<pre>"$5"</pre>"}' >> /var/www/html/index.html
